282a63c386de0a672780fba99543f5d6d526b1f8
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-16 23:47:50 +0300

    feat: add auth-api

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/layout';
 import { Card, Form, Row, Space, Typography } from 'antd';
 import { CustomInput } from '../../components/custom-input';
 import { CustomPasswordInput } from '../../components/custom-password-input';
 import { CustomButton } from '../../components/custom-button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths';
+import { useLoginMutation, UserData } from '../../app/services/auth';
+import { isErrorWithMessage } from '../../utils/is-error-with-message';
+import { ErrorMessage } from '../../components/error-message';
 
 export const Login = () => {
+  const navigate = useNavigate()
+	const [loginUser, loginUserResult] = useLoginMutation();
+	const [error, setError] = useState('');
+
+	const login = async (data: UserData) => {
+		try {
+			await loginUser(data).unwrap()
+			navigate('/')
+		} catch (e) {
+			const maybeError = isErrorWithMessage(e);
+
+			if (maybeError) {
+				setError(e.data.message);
+			} else {
+				console.log(e)
+				setError('Unknown error');
+			}
+		}
+	}
+
 	return (
 		<Layout>
 			<Row align="middle" justify="center">
 				<Card title="Sign In" style={ { width: '30rem' } }>
-					<Form onFinish={ () => null }>
+					<Form onFinish={ login }>
 						<CustomInput placeholder="Email" name="email" type="email" />
 
 						<CustomPasswordInput placeholder="Password" name="password" />
@@ -24,8 +47,11 @@ export const Login = () => {
 
 					<Space datatype="vertical" size="large">
 						<Typography.Text>
-							Do not have an account? <Link to={Paths.register}>Sign Up</Link>
+							Do not have an account? <Link to={ Paths.register }>Sign Up</Link>
 						</Typography.Text>
+
+						<ErrorMessage message={error}/>
+
 					</Space>
 
 				</Card>
