8ebf60c3bee39b305f5e2603b30262ea552b0b70
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-16 22:14:39 +0300

    fix: code style

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -14,17 +14,20 @@ export const Login = () => {
 				<Card title="Sign In" style={ { width: '30rem' } }>
 					<Form onFinish={ () => null }>
 						<CustomInput placeholder="Email" name="email" type="email" />
+
 						<CustomPasswordInput placeholder="Password" name="password" />
+
 						<CustomButton type="primary" htmlType="submit">
 							Sign In
 						</CustomButton>
 					</Form>
+
 					<Space datatype="vertical" size="large">
 						<Typography.Text>
 							Do not have an account? <Link to={Paths.register}>Sign Up</Link>
 						</Typography.Text>
-
 					</Space>
+
 				</Card>
 			</Row>
 		</Layout>
