f590a3ff43b77cf99a5465083f4217d2b8abea3b
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-16 22:15:04 +0300

    feat: add register component

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import Layout from '../../components/layout';
+import { Card, Form, Row, Space, Typography } from 'antd';
+import { CustomInput } from '../../components/custom-input';
+import { CustomPasswordInput } from '../../components/custom-password-input';
+import { CustomButton } from '../../components/custom-button';
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths';
 
 export const Register = () => {
 	return (
-		<div>
-			Register
-		</div>
+		<Layout>
+			<Row align="middle" justify="center">
+
+				<Card title="Sign Up" style={ { width: '30rem' } }>
+					<Form onFinish={ () => null }>
+						<CustomInput placeholder="Name" name="name" />
+
+						<CustomInput placeholder="Email" name="email" type="email" />
+
+						<CustomPasswordInput placeholder="Password" name="password" />
+
+						<CustomPasswordInput placeholder="Confirm password" name="confirmPassword" />
+
+						<CustomButton type="primary" htmlType="submit">
+							Sign Up
+						</CustomButton>
+					</Form>
+
+					<Space datatype="vertical" size="large">
+						<Typography.Text>
+							Are you already have an account? <Link to={ Paths.login }>Sign In</Link>
+						</Typography.Text>
+					</Space>
+
+				</Card>
+			</Row>
+		</Layout>
 	);
 };
