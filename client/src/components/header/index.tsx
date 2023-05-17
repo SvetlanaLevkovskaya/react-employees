e20ccf0a48dc4529727f4080b042096bceed4bb3
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-18 00:08:58 +0300

    feat: add header button logic

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Layout as AntLayout, Space, Typography } from 'antd';
 import styles from './index.module.css';
-import { LoginOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginOutlined, LogoutOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 import { CustomButton } from '../custom-button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../../features/auth/authSlice';
 
 export const Header = () => {
+	const user = useSelector(selectUser);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const onLogOutClick = () => {
+		dispatch(logout());
+		localStorage.removeItem('token')
+		navigate('/login')
+	}
+
 	return (
 		<AntLayout.Header className={ styles.header }>
 			<Space>
@@ -19,16 +31,19 @@ export const Header = () => {
 				</Link>
 
 			</Space>
-
+			{ user ? (
+				<CustomButton type="ghost" icon={ <LogoutOutlined /> } onClick={ onLogOutClick }>Log Out</CustomButton>
+			) : (
 			<Space>
 				<Link to={ Paths.register }>
-					<CustomButton type="ghost" icon={<UserOutlined/>}>Sign Up</CustomButton>
+					<CustomButton type="ghost" icon={ <UserOutlined /> }>Sign Up</CustomButton>
 				</Link>
 
 				<Link to={ Paths.login }>
-					<CustomButton type="ghost" icon={<LoginOutlined/>}>Sign Ip</CustomButton>
+					<CustomButton type="ghost" icon={ <LoginOutlined /> }>Sign Ip</CustomButton>
 				</Link>
 			</Space>
+			) }
 		</AntLayout.Header>
 	);
 };
