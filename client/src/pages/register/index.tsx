15e0ba39913590800c6353e480d8613734f75eaf
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-18 00:06:34 +0300

    feat: add register page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/layout';
 import { Card, Form, Row, Space, Typography } from 'antd';
 import { CustomInput } from '../../components/custom-input';
 import { CustomPasswordInput } from '../../components/custom-password-input';
 import { CustomButton } from '../../components/custom-button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths';
+import { UserData, useRegisterMutation } from '../../app/services/auth';
+import { isErrorWithMessage } from '../../utils/is-error-with-message';
 
 export const Register = () => {
+	const navigate = useNavigate()
+	const [registerUser] = useRegisterMutation();
+	const [error, setError] = useState('');
+
+	const register = async (data: UserData) => {
+		try {
+			await registerUser(data).unwrap()
+			navigate('/')
+		} catch (e) {
+			const maybeError = isErrorWithMessage(e);
+
+			if (maybeError) {
+				setError(e.data.message);
+			} else {
+				console.log(e)
+				setError('Unknown error');
+			}
+		}
+	}
 	return (
 		<Layout>
 			<Row align="middle" justify="center">
 
 				<Card title="Sign Up" style={ { width: '30rem' } }>
-					<Form onFinish={ () => null }>
+					<Form onFinish={ register }>
 						<CustomInput placeholder="Name" name="name" />
 
 						<CustomInput placeholder="Email" name="email" type="email" />
